be229659de55c65e0c91429f3dff98659fdeb5ec
Stephane BARON <baron.stephane@gmail.com>
2016-05-16 10:48:41 +0200

    Droits sur fichier de configuration

--- mattermost/scripts/entrypoint.sh
+++ mattermost/scripts/entrypoint.sh
@@ -11,35 +11,40 @@ fi
 # Data
 if [ ! -d $APP_HOME/data ]; then
 	mkdir $APP_HOME/data
-	chown $APP_USER $APP_HOME/data
+	chown $APP_USER:$APP_USER $APP_HOME/data
 fi
 
 # Logs
 if [ ! -d $APP_HOME/logs ]; then
 	mkdir $APP_HOME/logs
-	chown $APP_USER $APP_HOME/logs
+	chown $APP_USER:$APP_USER $APP_HOME/logs
 fi
 
-# Configuration
-if [ ! -f $APP_HOME/config.json ]; then
-	# Copie du template
-	cp /opt/mattermost/config/config.json $APP_HOME/config.json
-	chown $APP_USER $APP_HOME/config.json
+# Config
+if [ ! -d $APP_HOME/config ]; then
 
-	cat $APP_HOME/config.json | \
+	mkdir $APP_HOME/config
+	chown $APP_USER:$APP_USER $APP_HOME/config
+
+	# Copie du template
+	cp /opt/mattermost/config/config.json $APP_HOME/config/docker.json
+
+	cat $APP_HOME/config/docker.json | \
 		jq ".LogSettings.FileLocation = \"$APP_HOME/logs/app.log\"" | \
-		jq ".FileSettings.Directory = \"$APP_HOME/data\"" | \
-		jq ".ComplianceSettings.Directory = \"$APP_HOME/data\"" \
-		> $APP_HOME/config.json.tmp && \
-	mv $APP_HOME/config.json.tmp $APP_HOME/config.json
+		jq ".FileSettings.Directory = \"$APP_HOME/data\"" \
+		> $APP_HOME/config/docker.json.tmp && \
+	mv $APP_HOME/config/docker.json.tmp $APP_HOME/config/docker.json
 fi
 
 # Force db settings
-cat $APP_HOME/config.json | \
+cat $APP_HOME/config/docker.json | \
 	jq ".SqlSettings.DriverName = \"postgres\"" | \
  	jq ".SqlSettings.DataSource = \"postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DB?sslmode=disable&connect_timeout=10\"" \
-	> $APP_HOME/config.json.tmp && \
-mv $APP_HOME/config.json.tmp $APP_HOME/config.json
+	> $APP_HOME/config/docker.json.tmp && \
+mv $APP_HOME/config/docker.json.tmp $APP_HOME/config/docker.json
+
+# Config RW
+chown $APP_USER:$APP_USER $APP_HOME/config/docker.json
 
 # Waiting for db
 echo "Waiting for db ..."
@@ -48,5 +53,5 @@ do
   sleep 5
 done
 
-cd "/opt/mattermost/bin"
-exec gosu "$APP_USER" ./platform --config="$APP_HOME/config.json"
+cd "/opt/mattermost"
+exec gosu "$APP_USER" bin/platform --config="$APP_HOME/config/docker.json"
